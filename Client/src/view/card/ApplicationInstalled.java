85fef54a76decb31ece1e98213284ef6aea9816e
hoaithi-rk <phunghoaithi137@example.com>
2022-11-22 20:38:19 +0700

    Add running App, installed App

---
+++ Client/src/view/card/ApplicationInstalled.java
@@ -0,0 +1,184 @@
+package view.card;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ApplicationInstalled extends JPanel {
+    public ApplicationInstalled()
+    {
+        initComponents();
+    }
+
+    private void initComponents()
+    {
+        GridBagConstraints gBC;
+
+        setLayout(new GridBagLayout());
+        appButtonPanel = new JPanel();
+        listButton = new JButton();
+        startButton = new JButton();
+        stopButton = new JButton();
+        appInstalledListPanel = new JPanel();
+        appScrollPanel = new JScrollPane();
+        appInstalledListTable = new JTable();
+
+        appButtonPanel.setLayout(new GridBagLayout());
+
+        listButton.setBackground(new Color(40, 50, 65));
+        listButton.setFont(new Font("STXihei", -1, 22)); // NOI18N
+        listButton.setForeground(new Color(130, 197, 190));
+        listButton.setText("List");
+        gBC = new GridBagConstraints();
+        gBC.gridx = 0;
+        gBC.gridy = 0;
+        gBC.ipadx = 60;
+        gBC.ipady = 25;
+        gBC.weightx = 0.2;
+        gBC.weighty = 1.0;
+//        gBC.insets = new Insets(0, 50, 0, 0);
+
+        appButtonPanel.add(listButton, gBC);
+
+        startButton.setBackground(new Color(40, 50, 65));
+        startButton.setFont(new Font("STXihei", 0, 22)); // NOI18N
+        startButton.setForeground(new Color(130, 197, 190));
+        startButton.setText("Start");
+        gBC = new GridBagConstraints();
+        gBC.gridx = 1;
+        gBC.gridy = 0;
+        gBC.ipadx = 60;
+        gBC.ipady = 25;
+        gBC.weightx = 0.2;
+        gBC.weighty = 1.0;
+        appButtonPanel.add(startButton, gBC);
+
+        stopButton.setBackground(new Color(40, 50, 65));
+        stopButton.setFont(new Font("STXihei", -1, 22)); // NOI18N
+        stopButton.setForeground(new Color(130, 197, 190));
+        stopButton.setText("Stop");
+        gBC = new GridBagConstraints();
+        gBC.gridx = 2;
+        gBC.gridy = 0;
+        gBC.ipadx = 60;
+        gBC.ipady = 25;
+        gBC.weightx = 0.2;
+        gBC.weighty = 1.0;
+//        gBC.insets = new Insets(0, 0, 0, 200);
+        appButtonPanel.add(stopButton, gBC);
+
+        gBC = new GridBagConstraints();
+        gBC.gridx = 0;
+        gBC.gridy = 0;
+        gBC.fill = GridBagConstraints.BOTH;
+        gBC.weightx = 1;
+        gBC.weighty = 0.5;
+        gBC.insets = new Insets(0, 40, 0, 400);
+        add(appButtonPanel, gBC);
+
+        appInstalledListPanel.setLayout(new java.awt.GridBagLayout());
+
+//        appInstalledListTable.setDefaultEditor(Object.class, null);
+        appInstalledListTable.getTableHeader().setBackground(new java.awt.Color(40, 50, 65));
+        appInstalledListTable.getTableHeader().setForeground(new java.awt.Color(255,255,255));
+        appInstalledListTable.getTableHeader().setFont(new Font("STXihei", 0, 18));
+        appInstalledListTable.setFont(new java.awt.Font("STXihei", 0, 16)); // NOI18N
+        appInstalledListTable.setModel(new javax.swing.table.DefaultTableModel(
+                new Object [][] {
+                        { null, null},
+                        { null, null},
+                        { null, null},
+                        { null, null},
+                        { null, null},
+                        { null, null},
+                        { null, null},
+                        { null, null},
+                        { null, null},
+                        { null, null},
+                        { null, null}
+                },
+                headerAppInstall
+        ) {
+            Class[] types = new Class [] {
+                    java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                    false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+
+        appInstalledListTable.setRowHeight(30);
+        appInstalledListTable.getTableHeader().setReorderingAllowed(false);
+        appScrollPanel.setViewportView(appInstalledListTable);
+        if (appInstalledListTable.getColumnModel().getColumnCount() > 0) {
+            appInstalledListTable.getColumnModel().getColumn(0).setPreferredWidth(100);
+            appInstalledListTable.getColumnModel().getColumn(1).setPreferredWidth(300);
+        }
+        appInstalledListTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                clickedAppDirectory = appInstalledListTable.getValueAt(appInstalledListTable.getSelectedRow(), 1).toString();
+                System.out.println(clickedAppDirectory);
+            }
+        });
+
+        gBC = new java.awt.GridBagConstraints();
+        gBC.gridx = 0;
+        gBC.gridy = 0;
+        gBC.fill = java.awt.GridBagConstraints.BOTH;
+        gBC.weightx = 1.0;
+        gBC.weighty = 1.0;
+        appInstalledListPanel.add(appScrollPanel, gBC);
+
+        gBC = new GridBagConstraints();
+        gBC.gridx = 0;
+        gBC.gridy = 1;
+        gBC.fill = GridBagConstraints.BOTH;
+        gBC.weightx = 1.0;
+        gBC.weighty = 0.5;
+        add(appInstalledListPanel, gBC);
+    }
+
+    public JButton getListButton() {
+        return listButton;
+    }
+
+    public void updateAppInstalledPanel(ArrayList<ArrayList<String>> infoAppInstalled)
+    {
+        String[][] infoAppInstalledArray = infoAppInstalled.stream().map(u -> u.toArray(new String[0])).toArray(String[][]::new);
+
+        appInstalledListTable.repaint();
+        appInstalledListTable.setModel(new DefaultTableModel(infoAppInstalledArray, headerAppInstall));
+        if (appInstalledListTable.getColumnModel().getColumnCount() > 0) {
+            appInstalledListTable.getColumnModel().getColumn(0).setPreferredWidth(100);
+            appInstalledListTable.getColumnModel().getColumn(1).setPreferredWidth(300);
+        }
+    }
+
+    public String getClickedAppDirectory() {
+        return clickedAppDirectory;
+    }
+
+    private JButton listButton;
+    private JButton startButton;
+    private JButton stopButton;
+    private JPanel appButtonPanel;
+    private JPanel appInstalledListPanel;
+    private JScrollPane appScrollPanel;
+    private JTable appInstalledListTable;
+
+    private String[] headerAppInstall = {"Application", "Directory"};
+    private String clickedAppName;
+    private String clickedAppDirectory;
+}
---
+++ Client/src/view/card/ApplicationRunning.java
@@ -0,0 +1,163 @@
+package view.card;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ApplicationRunning extends JPanel {
+    public ApplicationRunning()
+    {
+        initComponents();
+    }
+
+    private void initComponents()
+    {
+        GridBagConstraints gBC;
+
+        setLayout(new GridBagLayout());
+        appButtonPanel = new JPanel();
+        listButton = new JButton();
+        startButton = new JButton();
+        stopButton = new JButton();
+        appListPanel = new JPanel();
+        appScrollPanel = new JScrollPane();
+        appListTable = new JTable();
+
+        appButtonPanel.setLayout(new GridBagLayout());
+
+        listButton.setBackground(new Color(40, 50, 65));
+        listButton.setFont(new Font("STXihei", -1, 22)); // NOI18N
+        listButton.setForeground(new Color(130, 197, 190));
+        listButton.setText("List");
+        gBC = new GridBagConstraints();
+        gBC.gridx = 0;
+        gBC.gridy = 0;
+        gBC.ipadx = 60;
+        gBC.ipady = 25;
+        gBC.weightx = 0.2;
+        gBC.weighty = 1.0;
+//        gBC.insets = new Insets(0, 50, 0, 0);
+
+        appButtonPanel.add(listButton, gBC);
+
+        startButton.setBackground(new Color(40, 50, 65));
+        startButton.setFont(new Font("STXihei", 0, 22)); // NOI18N
+        startButton.setForeground(new Color(130, 197, 190));
+        startButton.setText("Start");
+        gBC = new GridBagConstraints();
+        gBC.gridx = 1;
+        gBC.gridy = 0;
+        gBC.ipadx = 60;
+        gBC.ipady = 25;
+        gBC.weightx = 0.2;
+        gBC.weighty = 1.0;
+        appButtonPanel.add(startButton, gBC);
+
+        stopButton.setBackground(new Color(40, 50, 65));
+        stopButton.setFont(new Font("STXihei", -1, 22)); // NOI18N
+        stopButton.setForeground(new Color(130, 197, 190));
+        stopButton.setText("Stop");
+        gBC = new GridBagConstraints();
+        gBC.gridx = 2;
+        gBC.gridy = 0;
+        gBC.ipadx = 60;
+        gBC.ipady = 25;
+        gBC.weightx = 0.2;
+        gBC.weighty = 1.0;
+//        gBC.insets = new Insets(0, 0, 0, 200);
+        appButtonPanel.add(stopButton, gBC);
+
+        gBC = new GridBagConstraints();
+        gBC.gridx = 0;
+        gBC.gridy = 0;
+        gBC.fill = GridBagConstraints.BOTH;
+        gBC.weightx = 1;
+        gBC.weighty = 0.5;
+        gBC.insets = new Insets(0, 40, 0, 400);
+        add(appButtonPanel, gBC);
+
+        appListPanel.setLayout(new GridBagLayout());
+
+        appListTable.getTableHeader().setBackground(new Color(40, 50, 65));
+        appListTable.getTableHeader().setForeground(new Color(255,255,255));
+        appListTable.getTableHeader().setFont(new Font("STXihei", 0, 18));
+        appListTable.setFont(new Font("STXihei", 0, 16)); // NOI18N
+        appListTable.setModel(new javax.swing.table.DefaultTableModel(
+                new Object [][] {
+                        {"aaaa", "bbbbb", "ccccccc", "dddddddddd"},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+
+                },
+                new String [] {
+                        "Application", "ID", "Status", "Title 4"
+                }
+        ) {
+            Class[] types = new Class [] {
+                    String.class, String.class, String.class, String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                    false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+
+        appListTable.setRowHeight(30);
+        appListTable.getTableHeader().setReorderingAllowed(false);
+        appScrollPanel.setViewportView(appListTable);
+        if (appListTable.getColumnModel().getColumnCount() > 0) {
+            appListTable.getColumnModel().getColumn(0).setPreferredWidth(100);
+            appListTable.getColumnModel().getColumn(1).setPreferredWidth(30);
+            appListTable.getColumnModel().getColumn(2).setPreferredWidth(100);
+            appListTable.getColumnModel().getColumn(3).setPreferredWidth(100);
+        }
+
+        gBC = new GridBagConstraints();
+        gBC.gridx = 0;
+        gBC.gridy = 0;
+        gBC.fill = GridBagConstraints.BOTH;
+        gBC.weightx = 1.0;
+        gBC.weighty = 1.0;
+        appListPanel.add(appScrollPanel, gBC);
+
+        gBC = new GridBagConstraints();
+        gBC.gridx = 0;
+        gBC.gridy = 1;
+        gBC.fill = GridBagConstraints.BOTH;
+        gBC.weightx = 1.0;
+        gBC.weighty = 0.5;
+        add(appListPanel, gBC);
+    }
+
+    public JButton getListButton() {
+        return listButton;
+    }
+
+    private JButton listButton;
+    private JButton startButton;
+    private JButton stopButton;
+    private JPanel appButtonPanel;
+    private JPanel appListPanel;
+    private JScrollPane appScrollPanel;
+    private JTable appListTable;
+}
